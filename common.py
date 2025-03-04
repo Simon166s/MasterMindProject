bd3333198e9ddcd81332c87037ea54da1fa0df5f
Simon <simonvanbommel@gmail.com>
2025-03-04 17:41:37 +0100

    Ajout de la fonction donnes_possibles

--- common.py
+++ common.py
@@ -2,6 +2,8 @@
 
 LENGTH = 4
 COLORS = ['R', 'V', 'B', 'J', 'N', 'M', 'O', 'G']
+import itertools
+
 # Notez que vos programmes doivent continuer à fonctionner si on change les valeurs par défaut ci-dessus
 
 
@@ -33,6 +35,34 @@ def evaluation(arg, ref):
     return bien_places, mal_places
 
 
+#TO DO : Ajouter doc string et meilleur commentaires et tt
+
+def donner_possible(combinaison_teste,evaluation_associe):
+    bien_places,mal_places=evaluation_associe
+    #Faire un ensemble avec toutes les possibilites,
+    liste_ensemble_permutation=itertools.permutations(COLORS,LENGTH)
+    #Maintenant on va supprimer les combinaisons qui sont pas possibles et apres on aura l ensemble des combinaisons finales
+    
+    
+    liste_return = []
+    
+    
+    for element in liste_ensemble_permutation :
+        #On regarde si le nombre de meme couleur, si c'est egal a bien_places+ mal_places c'est deja bien
+        #Ensuite on regarde si le nombre de meme places = nombre bien places
+        bplaces,mplaces = evaluation(element,combinaison_teste)
+        if bplaces == bien_places and mplaces == mal_places :
+            liste_return.append(element)
+            
+    #print(liste_return)
+    return set(liste_return)
+
+
+#%% Partie Test
+
+donner_possible(['R', 'V', 'B', 'J'],evaluation(['R', 'V', 'B', 'J'], ['R', 'V', 'B', 'J']))
+
+
 argument = ['E', 'G', 'Y', 'L', 'C']
 ref = ['B', 'V', 'E', 'A', 'O']
 
@@ -60,6 +90,7 @@ evaluation(argument,ref)
 argument = ['J', 'C', 'O', 'C', 'T', 'B']
 ref = ['V', 'B', 'E', 'X', 'Q', 'G']
 evaluation(argument,ref)
+
 
               
         

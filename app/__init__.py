ac1ab14e1f00a7cb10f6a2ab7cc66b135b10356d
LeDuc <leandre.leduc38@gmail.com>
2025-03-18 09:24:07 +0100

    modify requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,21 @@
 from flask import Flask
-from  app.watch_scss import watch_scss
+# from  app.watch_scss import watch_scss
 import os
 import threading
 
 
-def start_scss_thread():
-    thread = threading.Thread(target = watch_scss, daemon=True)
-    thread.start()
+# def start_scss_thread():
+#     thread = threading.Thread(target = watch_scss, daemon=True)
+#     thread.start()
 
 
 def create_app():
     app = Flask(__name__)
     app.debug = False
     
-    # Lancer le watcher SCSS en mode debug
-    if app.debug:
-        start_scss_thread()
+    # # Lancer le watcher SCSS en mode debug
+    # if app.debug:
+    #     start_scss_thread()
 
     app.config['SECRET_KEY'] = 'ton_secret_key'
     from app.routes import main
